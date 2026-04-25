7ef69db44301af8afdd26e316698dfb3bbaa3700
tiwole <asteravve@gmail.com>
2026-04-25 17:47:05 +0300

    add skeleton to sections

--- src/Congratulator.WebAssembly/Components/HomeSection/HomeSection.razor.cs
+++ src/Congratulator.WebAssembly/Components/HomeSection/HomeSection.razor.cs
@@ -6,5 +6,8 @@ namespace Congratulator.WebAssembly.Components.HomeSection;
 public partial class HomeSection : BasePageComponent
 {
     [Parameter, EditorRequired] public string Title { get; set; } = null!;
-    [Parameter, EditorRequired] public List<PersonModel> People { get; set; } = null!;
+    [Parameter] public List<PersonModel> People { get; set; } = [];
+    [Parameter] public bool IsLoading { get; set; }
+
+    private readonly int _skeletonCount = Random.Shared.Next(1, 7);
 }
