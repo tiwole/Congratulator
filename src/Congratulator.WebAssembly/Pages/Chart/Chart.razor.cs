92223104510efd5011b5b7a074a0d8edfcb6821d
tiwole <asteravve@gmail.com>
2026-04-25 12:53:17 +0300

    init

---
+++ src/Congratulator.WebAssembly/Pages/Chart/Chart.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Congratulator.WebAssembly.Pages.Chart;
+
+public partial class Chart : ComponentBase
+{
+}
---
+++ src/Congratulator.WebAssembly/Pages/Chart/Chart.razor.js
@@ -0,0 +1,5 @@
+﻿export class Chart {
+  
+}
+
+window.Chart = Chart;
