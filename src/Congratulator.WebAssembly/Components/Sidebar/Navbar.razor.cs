04562693263e588b3eb664231544e017c5ee8048
tiwole <asteravve@gmail.com>
2026-04-25 18:55:01 +0300

    add confetti when click logo

--- src/Congratulator.WebAssembly/Components/Sidebar/Navbar.razor.cs
+++ src/Congratulator.WebAssembly/Components/Sidebar/Navbar.razor.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Congratulator.WebAssembly.Components.Sidebar;
 
 public partial class Navbar : BasePageComponent
 {
-    [Parameter]
-    [EditorRequired]
-    public string Title { get; set; } = "";
-    
-    [Parameter] 
-    [EditorRequired]
-    public List<SidebarItem> Items { get; set; } = [];
+    [Parameter, EditorRequired] public string Title { get; set; } = "";
+    [Parameter, EditorRequired] public List<SidebarItem> Items { get; set; } = [];
+
+    private ElementReference _brandRef;
+
+    private async Task OnBrandClick()
+    {
+        await JsRuntime.InvokeVoidAsync("launchConfetti", _brandRef);
+    }
 }
