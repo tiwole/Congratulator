3c36e1b3e2986db0ff5ee5fcb4835d81a7e5a451
tiwole <asteravve@gmail.com>
2026-04-26 14:37:14 +0300

    add background

--- src/Congratulator.WebAssembly/Components/PersonDetails/PersonDetails.razor.cs
+++ src/Congratulator.WebAssembly/Components/PersonDetails/PersonDetails.razor.cs
@@ -1,3 +1,4 @@
+using Congratulator.SharedKernel.Contracts.Enums;
 using Congratulator.SharedKernel.Contracts.Models;
 using Microsoft.AspNetCore.Components;
 
@@ -9,4 +10,13 @@ public partial class PersonDetails : BasePageComponent
     public PersonModel Person { get; set; } = null!;
 
     private string GetName(PersonModel person) => person.LastName is null ? person.FirstName : $"{person.FirstName} {person.LastName}";
+
+    private string GetRelationshipColorVar() => Person.RelationshipType switch
+    {
+        RelationshipType.Family => "--lumex-warning",
+        RelationshipType.Friend => "--lumex-success",
+        RelationshipType.Mate => "--lumex-secondary",
+        RelationshipType.Coworker => "--lumex-primary",
+        _ => "--lumex-default"
+    };
 }
